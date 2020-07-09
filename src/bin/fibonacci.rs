3e58619c55bf2117d2bcf3e1b42c09ceab6ff672
luojia65 <me@luojia.cc>
2020-07-10 00:11:19 +0800

    Fibonacci calculator

---
+++ src/bin/fibonacci.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate libspicyos;
+// use libspicyos as std;
+
+#[export_name = "main"]
+fn main() {
+    println!("Start fibonacci!");
+    let n = 10;
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 0..n {
+        a += b;
+        core::mem::swap(&mut a, &mut b);
+    }
+    println!("Fibonacci sequence #{} is {}", n, a);
+}
